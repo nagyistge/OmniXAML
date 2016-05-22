c8c1cd62991ccb767a1c25237c74cb6da631d288
José Manuel Nieto Sánchez <superjmn@outlook.com>
2016-05-22 17:47:41 +0200

    Más cambios guapos

--- Source/OmniXaml.Wpf/WpfXamlLoaderTypeFactory.cs
+++
@@ -1,9 +0,0 @@
-namespace OmniXaml.Wpf
-{
-    public class WpfXamlLoaderTypeFactory : AlternateOnRootTypeFactory
-    {
-        public WpfXamlLoaderTypeFactory() : base(new TypeFactory(), new WpfTypeFactory())
-        {
-        }
-    }
-}
